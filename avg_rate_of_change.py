9d7b326ebcc948ad489ac0e2697229023514b85a
GK <jpkutner+github@gmail.com>
2022-06-19 18:39:23 -0500

    added support for fractions in avg rate of change

--- avg_rate_of_change.py
+++ avg_rate_of_change.py
@@ -1,31 +1,30 @@
 import slope
+from fractions import Fraction
 
 def apply_polynomial(coefficients, x):
 	value = 0
 	for i in range(len(coefficients)):
 		c = coefficients[i]
 		p = c * (x**i)
-		print("%d * %d ^ %d = %d" % (c, x, i, p))
-		#print("%d * %d = %d" % (c, (x**i), p))
 		value += p
 	return value
 
 if __name__ == "__main__":
-	o = int(input("Enter order: "))
+	o = int(input("Enter degree: "))
 	coefficients = []
-	constant = int(input("Enter constant: "))
+	constant = Fraction(input("Enter constant: "))
 	coefficients.append(constant)
 	print("Enter coefficients: ")
-	r = range(1, o)
+	r = range(o)
 	for i in r:
-		c = int(input("  x^%d: " % i))
+		c = Fraction(input("  x^{}: ".format(i + 1)))
 		coefficients.append(c)
 	print("Enter interval: ")
-	x1 = int(input("  Start: "))
-	x2 = int(input("  Finish: "))
+	x1 = Fraction(input("  Start: "))
+	x2 = Fraction(input("  Finish: "))
 	y1 = apply_polynomial(coefficients, x1)
 	y2 = apply_polynomial(coefficients, x2)
 	print()
-	print("points: (%d, %d), (%d, %d)" % (x1, y1, x2, y2))
+	print("points: ({}, {}), ({}, {})".format(x1, y1, x2, y2))
 	s = slope.find_slope(x1, y1, x2, y2)
-	print("average rate of change = %d" % s)
+	print("average rate of change = {}".format(s))
